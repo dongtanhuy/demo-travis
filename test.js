cea800ba822c0f020c9b8b01fc8b0005cdafd10f
Đồng Tấn Huy <dongtanhuy@gmail.com>
2017-03-08 16:09:03 +0700

    unit test true

--- test.js
+++ test.js
@@ -4,6 +4,6 @@ var add = require('./index')
 // Start testing add function
 describe('ADD Function', function () {
   it('should return 3 when params = (1,2)', function () {
-    add(1, 2).should.eql(4)
+    add(1, 2).should.eql(3)
   })
 })
